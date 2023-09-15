cf99d5ca159da95084f2bfe9fa6482ad261f7a1e
TacoConKvass <128845692+TacoConKvass@users.noreply.github.com>
2023-09-15 16:56:49 +0200

    Update to include drag

--- c++/main.cpp
+++ c++/main.cpp
@@ -9,13 +9,16 @@
 #include <cstring>
 #include <string>
 
+bool first = true;
+double side;
 double cosT;
 double sinT;
 
-int updateTimer = 1;
 double LINE_LENGTH = .75;
 double PIVOT_POS[2] = { .0, .5 };
-double GRAVITY_CONST = 9.801;
+double GRAVITY_CONST = 9.8203;
+double AIR_DENSITY = 1.225;     // Average value at sea level
+double DRAG_COEFFICIENT = .47;  // Aproximate for a sphere
 
 double weightCenter[2];
 double weightMass = 1.;
@@ -24,16 +27,18 @@ double gravity_val = GRAVITY_CONST * weightMass / 1000;
 double tension_val;
 double acceleration_val;
 double velocity_val;
+double drag_val = .1;
 
 double gravity[2] = { .0, -gravity_val };
 double tension[2];
 double acceleration[2];
 double velocity[2];
+double drag[2];
 
-void text_out(int x, int y, float r, float g, float b, char* string)
+void text_out(double x, double y, double r, double g, double b, char* string)
 {
-    glColor3f(r, g, b);
-    glRasterPos2f(x, y);
+    glColor3d(r, g, b);
+    glRasterPos2d(x, y);
     int len, i;
     len = (int)strlen(string);
     for (i = 0; i < len; i++) {
@@ -43,9 +48,15 @@ void text_out(int x, int y, float r, float g, float b, char* string)
 
 void updatePendulum()
 {
+    side = -1.;
     if (weightCenter[1] < -.251)
     {
         weightCenter[1] = -.25;
+    }
+    
+    if (velocity_val < .0)
+    {
+        side = 1.;
     }
 
     cosT = floor(weightCenter[0] / LINE_LENGTH * 100) / 100;
@@ -59,9 +70,14 @@ void updatePendulum()
     acceleration[0] = sinT * acceleration_val;
     acceleration[1] = cosT * acceleration_val;
 
-    velocity_val += acceleration_val;
-    velocity[0] = sinT * velocity_val / 2500;
-    velocity[1] = cosT * velocity_val / 2500;
+    velocity_val += acceleration_val/ 2500;
+    velocity_val += drag_val / 2500.;
+    velocity[0] = sinT * velocity_val;
+    velocity[1] = cosT * velocity_val;
+
+    drag_val = .5 * AIR_DENSITY * DRAG_COEFFICIENT * pow(velocity_val * 200, 2) * 3.14 * .01 * side;
+    drag[0] = sinT * drag_val;
+    drag[1] = cosT * drag_val;
 
     weightCenter[0] += velocity[0];
     weightCenter[1] += velocity[1];
@@ -87,14 +103,10 @@ void display() {
     // Set every pixel in the frame buffer to the current clear color.
     glClear(GL_COLOR_BUFFER_BIT);
 
-    if (updateTimer == 0 && weightCenter[1] > -.27)
-    {
-        updatePendulum();
-        updateTimer = 1;
-    }
-    updateTimer--;
+    updatePendulum();
 
     glBegin(GL_LINES);
+    
     glColor3d(1., 1., 1.);
     glVertex2d(weightCenter[0], weightCenter[1]);
     glVertex2d(PIVOT_POS[0], PIVOT_POS[1]);
@@ -103,17 +115,23 @@ void display() {
     glVertex2d(weightCenter[0], weightCenter[1]);
     glVertex2d(weightCenter[0] + gravity[0] * 10., weightCenter[1] + gravity[1] * 10.);
 
+    /*
     glColor3d(1., 1., .0);
     glVertex2d(weightCenter[0], weightCenter[1]);
     glVertex2d(weightCenter[0] + acceleration[0] * 10., weightCenter[1] + acceleration[1] * 10.);
+    */
 
     glColor3d(.0, 1., .0);
     glVertex2d(weightCenter[0], weightCenter[1]);
     glVertex2d(weightCenter[0] + tension[0] * 10., weightCenter[1] + tension[1] * 10.);
+    
+    glColor3d(1., .0, .0);
+    glVertex2d(weightCenter[0], weightCenter[1]);
+    glVertex2d(weightCenter[0] + drag[0] * 100., weightCenter[1] + drag[1] * 100.);
 
     glColor3d(.0, .0, 1.);
     glVertex2d(weightCenter[0], weightCenter[1]);
-    glVertex2d(weightCenter[0] + velocity[0] * 10., weightCenter[1] + velocity[1] * 10.);
+    glVertex2d(weightCenter[0] + velocity[0] * 200., weightCenter[1] + velocity[1] * 200.);
     
     glEnd();
 
@@ -132,7 +150,17 @@ void display() {
     char* char_array = new char[text_len + 1];
     strcpy(char_array, text.c_str());
 
-    text_out(-1, -1, 1., 1., 1., char_array);
+    std::string text2 = "Velocity: ";
+    text2 += std::to_string(velocity_val);
+    text2 += "      Drag: ";
+    text2 += std::to_string(drag_val);
+
+    int text2_len = text2.length();
+    char* char_array2 = new char[text2_len + 1];
+    strcpy(char_array2, text2.c_str());
+
+    text_out(-1., .95, 1., 1., 1., char_array);
+    text_out(-1., .9, 1., 1., 1., char_array2);
 
     // Flush drawing command buffer to make drawing happen as soon as possible.
     glFlush();
@@ -151,8 +179,8 @@ void keyboard(unsigned char key, int x, int y)
 
 int main(int argc, char** argv)
 {
-    weightCenter[0] = .74;
-    weightCenter[1] = .378;
+    weightCenter[0] = .75;
+    weightCenter[1] = .5;
 
     // Use a single buffered window in RGB mode (as opposed to a double-buffered
     // window or color-index mode).
